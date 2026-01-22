9f5e90f900b5711f614ddd623be128a81e86a57d
aquariax <roberthrrs8@gmail.com>
2026-01-22 00:42:33 -0700

    updated website links

--- src/parts/layout/navbar/NavDropdown.js
+++ src/parts/layout/navbar/NavDropdown.js
@@ -3,19 +3,20 @@ import NavDropdownItem from './NavDropdownitem';
 import { NavLink } from 'react-router-dom';
 
 
+
 	
 const NavDropdown = (props) => {
 	return (
 		<div className={props.texts}>
 			
 			<div className='nav-menu'>
-				<NavDropdownItem><NavLink to='/'><p className='menu-nav_link'>Home</p></NavLink></NavDropdownItem>
-				<NavDropdownItem><NavLink to='/wedding_day'><p className='menu-nav_link'>Wedding Day</p></NavLink></NavDropdownItem>
-				<NavDropdownItem><NavLink to='/our_story'><p className='menu-nav_link'>Our Story</p></NavLink></NavDropdownItem>
-				<NavDropdownItem><NavLink to='/travel'><p className='menu-nav_link'>Travel</p></NavLink></NavDropdownItem>
-				<NavDropdownItem><NavLink to='/gift_registry'><p className='menu-nav_link'>Gift Registry</p></NavLink></NavDropdownItem>
-				<NavDropdownItem><NavLink to='/FAQ'><p className='menu-nav_link'>Q&A</p></NavLink></NavDropdownItem>
-				<NavDropdownItem><NavLink to='/RSVP'><p className='menu-nav_link'>Rsvp</p></NavLink></NavDropdownItem>
+				<NavDropdownItem><NavLink to="https://lexyandrobs.netlify.app//"><p className='menu-nav_link'>Home</p></NavLink></NavDropdownItem>
+				<NavDropdownItem><NavLink to='https://lexyandrobs.netlify.app//wedding_day'><p className='menu-nav_link'>Wedding Day</p></NavLink></NavDropdownItem>
+				<NavDropdownItem><NavLink to='https://lexyandrobs.netlify.app//our_story'><p className='menu-nav_link'>Our Story</p></NavLink></NavDropdownItem>
+				<NavDropdownItem><NavLink to='https://lexyandrobs.netlify.app//travel'><p className='menu-nav_link'>Travel</p></NavLink></NavDropdownItem>
+				<NavDropdownItem><NavLink to='https://lexyandrobs.netlify.app//gift_registry'><p className='menu-nav_link'>Gift Registry</p></NavLink></NavDropdownItem>
+				<NavDropdownItem><NavLink to='https://lexyandrobs.netlify.app//FAQ'><p className='menu-nav_link'>Q&A</p></NavLink></NavDropdownItem>
+				<NavDropdownItem><NavLink to='https://lexyandrobs.netlify.app//RSVP'><p className='menu-nav_link'>Rsvp</p></NavLink></NavDropdownItem>
 			</div>
 		</div>
 	);
--- src/parts/layout/navbar/NavLinks.js
+++ src/parts/layout/navbar/NavLinks.js
@@ -4,13 +4,13 @@ const NavLinks = () => {
 	return (
 		<div className='nav-links'>
 			<div className='nav-auth'>
-				<NavLink to='/'><p className='menu-nav_link'>Home</p></NavLink>
-				<NavLink to='/wedding_day'><p className='menu-nav_link'>Wedding Day</p></NavLink>
-				<NavLink to='/our_story'><p className='menu-nav_link'>Our Story</p></NavLink>
-				<NavLink to='/travel'><p className='menu-nav_link'>Travel</p></NavLink>
-				<NavLink to='/gift_registry'><p className='menu-nav_link'>Gift Registry</p></NavLink>
-				<NavLink to='/FAQ'><p className='menu-nav_link'>Q&A</p></NavLink>
-				<NavLink to='/RSVP'><p className='menu-nav_link'>Rsvp</p></NavLink>
+				<NavLink to='https://lexyandrobs.netlify.app//'><p className='menu-nav_link'>Home</p></NavLink>
+				<NavLink to='https://lexyandrobs.netlify.app//wedding_day'><p className='menu-nav_link'>Wedding Day</p></NavLink>
+				<NavLink to='https://lexyandrobs.netlify.app//our_story'><p className='menu-nav_link'>Our Story</p></NavLink>
+				<NavLink to='https://lexyandrobs.netlify.app//travel'><p className='menu-nav_link'>Travel</p></NavLink>
+				<NavLink to='https://lexyandrobs.netlify.app//gift_registry'><p className='menu-nav_link'>Gift Registry</p></NavLink>
+				<NavLink to='https://lexyandrobs.netlify.app//FAQ'><p className='menu-nav_link'>Q&A</p></NavLink>
+				<NavLink to='https://lexyandrobs.netlify.app//RSVP'><p className='menu-nav_link'>Rsvp</p></NavLink>
 			</div>
 		</div>
 	);
